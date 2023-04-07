a3b54250dfee0c6166b1f730b23f31444f1edfc3
Ilshat Aminev <i.aminev@gmail.com>
2023-04-07 15:43:40 +0300

    task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,6 +3,16 @@
 //a = 2 b = 10->max = 10
 //a = -9 b = -3->max = -3
 
-max = 0;
 
-Console.WriteLine($"Max = {max}");
+// РЕШЕНИЕ
+// так нет в постановке задачи нет уточнения, что именно могут быть за числа, берем по максимуму
+Console.Write("Enter number 1: ");
+double number1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Enter number 2: ");
+double number2 = Convert.ToDouble(Console.ReadLine()!);
+
+if (number2 > number1)
+    Console.WriteLine($"Max = {number2}");
+else
+    Console.WriteLine($"Max = {number1}");
